0e7fde5b3697864333d259a8f5da89399c0e8217
Abdul Majid <majidsaddiqye@gmail.com>
2025-11-02 01:40:15 +0500

    Add Back Button Feacture

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let allElems = document.querySelectorAll(".elem");
 let allfullElems = document.querySelectorAll(".fullElem");
+let allfullElemsBackbtn = document.querySelectorAll(".fullElem .back");
 allElems.forEach(function (elem) {
   elem.addEventListener("click", function () {
     allfullElems[elem.id].style.display = "block";
+  });
+});
+
+allfullElemsBackbtn.forEach(function (back) {
+  back.addEventListener("click", function () {
+    allfullElems[back.id].style.display = "none";
   });
 });
