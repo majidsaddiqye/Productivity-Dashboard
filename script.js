a2c34eafb3c82028cd41e79e8ae503a0162d98f7
Abdul Majid <majidsaddiqye@gmail.com>
2025-11-03 00:45:43 +0500

    Complete Daily Planner full page

--- script.js
+++ script.js
@@ -73,3 +73,40 @@ function taskList() {
   });
 }
 taskList();
+
+function dailyPlanner() {
+  var dayPlanner = document.querySelector(".day-planner");
+
+  var dayPlanData = JSON.parse(localStorage.getItem("dayPlanData")) || {};
+
+  var hours = Array.from(
+    { length: 18 },
+    (_, idx) => `${6 + idx}:00 - ${7 + idx}:00`
+  );
+
+  var wholeDaySum = "";
+  hours.forEach(function (elem, idx) {
+    var savedData = dayPlanData[idx] || "";
+
+    wholeDaySum =
+      wholeDaySum +
+      `<div class="day-planner-time">
+    <p>${elem}</p>
+    <input id=${idx} type="text" placeholder="..." value=${savedData}>
+</div>`;
+  });
+
+  dayPlanner.innerHTML = wholeDaySum;
+
+  var dayPlannerInput = document.querySelectorAll(".day-planner input");
+
+  dayPlannerInput.forEach(function (elem) {
+    elem.addEventListener("input", function () {
+      console.log("hello");
+      dayPlanData[elem.id] = elem.value;
+
+      localStorage.setItem("dayPlanData", JSON.stringify(dayPlanData));
+    });
+  });
+}
+dailyPlanner();
